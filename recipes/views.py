c121d62d8c6aafc4d49c77297aa6a2e9578cd9f7
Thales <thales.lima13@hotmail.com>
2023-08-03 10:54:52 -0300

    refactor: The HTML pages were moved to the correct paths, and the HTML head was separated to be used in many pages.

--- recipes/views.py
+++ recipes/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
 
 
 def about(request):
-    return render(request, 'recipes/about.html')
+    return render(request, 'recipes/pages/about.html')
